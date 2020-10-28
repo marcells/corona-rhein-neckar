8629ee5547e5ae3ca40af9dcfacf0721683b4796
Marcell Spies <marcells@gmx.de>
2020-10-28 22:14:05 +0100

    Automatically preload cache every hour for faster first time access.

--- index.js
+++ index.js
@@ -17,8 +17,34 @@ app.put('/api/reset', async (_, res) => {
     res.sendStatus(200);
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Corona-Scraper listening at http://localhost:${port}`)
     console.log();
-    console.log(`Open your browser on http://localhost:${port} to see the current statistics.`)
+    console.log(`Open your browser on http://localhost:${port} to see the current statistics.`);
+    console.log();
+    await startCacheRefresher();
 });
+
+export const startCacheRefresher = async () => {
+  await preLoadData();
+  scheduleTimeout();
+
+  function getIntervalUntilNextHour () {
+    return 3600000 - new Date().getTime() % 3600000;
+  }
+
+  function scheduleTimeout () {
+    console.log(`Next automatic cache update in ${getIntervalUntilNextHour() / 1000} seconds...`);
+
+    setTimeout(async () => {
+      await preLoadData();
+
+      scheduleTimeout();
+    }, getIntervalUntilNextHour());
+  }
+
+  async function preLoadData() {
+    console.log('Preloading data...');
+    await loadData();
+  }
+};
