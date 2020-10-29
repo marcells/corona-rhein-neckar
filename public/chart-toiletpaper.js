5d3e6cdf64e850b8315e06580f71b04b61512496
Marcell Spies <marcells@gmx.de>
2020-10-29 21:54:38 +0100

    Display toilet paper per habitant.

--- public/chart-toiletpaper.js
+++ public/chart-toiletpaper.js
@@ -4,6 +4,7 @@ registerChart('chart-toiletpaper', async chartLink => {
 
   const cities = data.stats.toiletPaperPerCity.map(x => x.interest);
   const amountOfToiletPaper = data.stats.toiletPaperPerCity.map(x => x.amount);
+  const amountOfToiletPaperPerHabitant = data.stats.toiletPaperPerCity.map(x => x.amount === null ? null : x.amount / x.interest.numberOfHabitants);
   const sevenDayPer100000 = data.stats.infectionsPerCity.map(x => x.sevenDayPer100000);
   const currentInfections = data.stats.infectionsPerCity.map(x => x.currentInfections);
   const totalInfections = data.stats.infectionsPerCity.map(x => x.totalInfections);
@@ -17,7 +18,7 @@ registerChart('chart-toiletpaper', async chartLink => {
       text: chartLink.chartName
     },
     subtitle: {
-      text: '<b>Daten liegen leider nicht für alle Städte vor</b>'
+      text: '<b>❗ Die Daten beziehen nur auf DM-Drogerien und liegen leider nicht für alle Städte vor. ❗</b>'
     },
     xAxis: {
       categories: cities,
@@ -37,6 +38,19 @@ registerChart('chart-toiletpaper', async chartLink => {
       labels: {
         style: {
           color: Highcharts.getOptions().colors[0]
+        }
+      },
+    }, {
+      min: 0,
+      title: {
+        text: 'Verfügbare Packungen pro Einwohner',
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      labels: {
+        style: {
+          color: Highcharts.getOptions().colors[1]
         }
       },
     }, {
@@ -62,24 +76,32 @@ registerChart('chart-toiletpaper', async chartLink => {
     },
     series: [{
       yAxis: 0,
-      name: 'Toilettenpapier (DM-Supermärkte)',
+      name: 'Verfügbares Toilettenpapier',
       data: amountOfToiletPaper,
       tooltip: {
         valueDecimals: 0,
-        valueSuffix: ' verfügbare Packungen'
+        valueSuffix: ' Packungen'
       }
     }, {
       yAxis: 1,
+      name: 'Verfügbares Toilettenpapier pro Einwohner',
+      data: amountOfToiletPaperPerHabitant,
+      tooltip: {
+        valueDecimals: 5,
+        valueSuffix: ' Packungen'
+      }
+    }, {
+      yAxis: 2,
       name: 'Gesamte Infektionen',
       data: totalInfections,
       visible: false,
     }, {
-      yAxis: 1,
+      yAxis: 2,
       name: 'Aktuell Infizierte',
       data: currentInfections,
       visible: false,
     }, {
-      yAxis: 1,
+      yAxis: 2,
       name: '7-Tage-Inzidenz',
       data: sevenDayPer100000,
       visible: false,
